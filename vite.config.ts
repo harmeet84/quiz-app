2f9766c98c08e1ea8261f5efafe10b493d900788
Harmeet <35918713+harmeet84@users.noreply.github.com>
2025-09-21 15:58:43 +0100

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'node:path'
+
+export default defineConfig({
+  plugins: [react()],
+
+  resolve: { alias: { '@': path.resolve(__dirname, './src') } },
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://s3.eu-west-2.amazonaws.com/interview.mock.data',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
+})
