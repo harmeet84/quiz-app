c61fd95347dc61a05dd36c7d8c9b306ede9ae81d
Harmeet <35918713+harmeet84@users.noreply.github.com>
2025-09-21 16:09:52 +0100

    Delete index.ts

--- index.ts
+++
@@ -1,6 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import quizReducer from './quizSlice'
-
-export const store = configureStore({ reducer: { quiz: quizReducer } })
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
